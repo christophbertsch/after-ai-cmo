4800b7f95d6fbdb4af1bf6cf5d728fb3266a01bd
christophbertsch <c@camm.org>
2025-04-27 18:58:24 +0200

    Delete pages/api directory

--- pages/api/test.js
+++
@@ -1,9 +0,0 @@
-// pages/api/test.js
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    return res.status(200).json({ message: '✅ Server is alive and working!' });
-  } else {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
